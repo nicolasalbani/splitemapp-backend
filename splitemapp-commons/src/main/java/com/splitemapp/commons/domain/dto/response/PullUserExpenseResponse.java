ca16bca2e24866069f0ba9b1e12ccd12799800a1
Nicolas Albani <nicolasalbani@gmail.com>
2015-01-19 00:48:20 -0300

    Renaming

--- splitemapp-commons/src/main/java/com/splitemapp/commons/domain/dto/response/PullUserExpenseResponse.java
+++ splitemapp-commons/src/main/java/com/splitemapp/commons/domain/dto/response/PullUserExpenseResponse.java
@@ -2,6 +2,6 @@ package com.splitemapp.commons.domain.dto.response;
 
 import com.splitemapp.commons.domain.dto.UserExpenseDTO;
 
-public class PullUserExpenseResult extends PullResponse<UserExpenseDTO> {
+public class PullUserExpenseResponse extends PullResponse<UserExpenseDTO> {
 	private static final long serialVersionUID = -188442756704128607L;
 }
