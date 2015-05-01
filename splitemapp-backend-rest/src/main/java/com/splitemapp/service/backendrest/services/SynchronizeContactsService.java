5489f063e349c5dce87d36325d15c1755aba2058
Nicolas Albani <nicolasalbani@gmail.com>
2015-05-01 20:22:10 -0300

    Adding syncrhonize contacts logic

---
+++ splitemapp-backend-rest/src/main/java/com/splitemapp/service/backendrest/services/SynchronizeContactsService.java
@@ -0,0 +1,87 @@
+package com.splitemapp.service.backendrest.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.stereotype.Service;
+
+import com.splitemapp.commons.constants.ServiceConstants;
+import com.splitemapp.commons.constants.TableField;
+import com.splitemapp.commons.domain.User;
+import com.splitemapp.commons.domain.UserContactData;
+import com.splitemapp.commons.domain.dto.UserContactDataDTO;
+import com.splitemapp.commons.domain.dto.UserDTO;
+import com.splitemapp.commons.domain.dto.request.SynchronizeContactsRequest;
+import com.splitemapp.commons.domain.dto.response.SynchronizeContactsResponse;
+import com.splitemapp.commons.utils.Utils;
+import com.splitemapp.service.backendrest.endpoint.UserContactDataEndpoint;
+import com.splitemapp.service.backendrest.endpoint.UserEndpoint;
+
+@Service
+@Path(ServiceConstants.SYNCHRONIZE_CONTACTS_PATH)
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SynchronizeContactsService {
+
+	private UserEndpoint userEndpoint;
+	private UserContactDataEndpoint userContactDataEndpoint;
+
+	@GET
+	public String printMessage() {
+		return this.getClass().getSimpleName() +" - "+ ServiceConstants.GET_SUCCESS;
+	}
+
+	@POST
+	public SynchronizeContactsResponse printMessage(SynchronizeContactsRequest request) {
+
+		// Creating a synchronize contacts response object setting success to false by default
+		SynchronizeContactsResponse synchronizeContactsResponse = new SynchronizeContactsResponse();
+		synchronizeContactsResponse.setSuccess(false);
+
+		// Setting the references for the User and UserContactData DTO lists
+		List<UserDTO> userDTOList = new  ArrayList<UserDTO>();
+		synchronizeContactsResponse.setUserDTOList(userDTOList);
+		List<UserContactDataDTO> userContactDataDTOList = new  ArrayList<UserContactDataDTO>();
+		synchronizeContactsResponse.setUserContactDataDTOList(userContactDataDTOList);
+
+		// Adding all found User and UserContactData information to the response list
+		for(String emailAddress:request.getContactsEmailAddressList()){
+			// Processing user contact data information
+			UserContactData userContactData = userContactDataEndpoint.findByField(Utils.getEntityName(TableField.USER_CONTACT_DATA_CONTACT_DATA), emailAddress);
+			userContactDataDTOList.add(new UserContactDataDTO(userContactData));
+			
+			// Processing user information
+			User user = userContactData.getUser();
+			userDTOList.add(new UserDTO(user));
+		}
+
+		// Setting the success flag to true
+		synchronizeContactsResponse.setSuccess(true);
+
+		return synchronizeContactsResponse;
+	}
+
+	//Getters and setters
+	public UserEndpoint getUserEndpoint() {
+		return userEndpoint;
+	}
+
+	public void setUserEndpoint(UserEndpoint userEndpoint) {
+		this.userEndpoint = userEndpoint;
+	}
+
+	public UserContactDataEndpoint getUserContactDataEndpoint() {
+		return userContactDataEndpoint;
+	}
+
+	public void setUserContactDataEndpoint(UserContactDataEndpoint userContactDataEndpoint) {
+		this.userContactDataEndpoint = userContactDataEndpoint;
+	}
+}
